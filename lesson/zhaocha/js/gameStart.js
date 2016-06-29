1e228d46ab08643544bc25bc8761b5ea29756a9c
unknown <>
2016-06-29 14:09:25 +0800

    zhaocha game

---
+++ lesson/zhaocha/js/gameStart.js
@@ -0,0 +1,33 @@
+$('.start').tap(function() {
+	$('#content').show();
+	init();
+	$(this).unbind('tap')
+})
+
+function init() {
+	game.play();
+	game.count(function() {
+		game.countTimer(15);
+	});
+	game.creatImg()
+	game.click();
+}
+
+function next(id) {
+	$('#allwin').removeClass('show');
+	$(id).addClass('show');
+}
+
+$('#nextSub').on('tap', function() {
+	$('#nextWindow').removeClass('show')
+	game.again(15);
+})
+
+$('#again').on('tap', function() {
+	$('.again').removeClass('show');
+	game.again(15);
+})
+
+$('#post').on('tap', function() {
+	$('#phone').removeClass('show')
+})
